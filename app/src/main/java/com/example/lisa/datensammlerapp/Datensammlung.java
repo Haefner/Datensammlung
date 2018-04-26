639568460756c8ef8e51a64dffeba0c2421d50b8
Haefner <lisa-maria.haefner@hs-bochum.de>
2018-04-26 17:24:21 +0200

    Projekt erstanlage

---
+++ app/src/main/java/com/example/lisa/datensammlerapp/Datensammlung.java
@@ -0,0 +1,13 @@
+package com.example.lisa.datensammlerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Datensammlung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datensammlung);
+    }
+}
