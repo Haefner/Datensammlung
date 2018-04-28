b7d6e8f2fec4aa46e583ceff1c7a6f1e0f76a962
fefe131 <38761388+fefe131@users.noreply.github.com>
2018-04-28 11:57:20 +0200

    init der Gui Elemente

--- app/src/main/java/com/example/lisa/datensammlerapp/Datensammlung.java
+++ app/src/main/java/com/example/lisa/datensammlerapp/Datensammlung.java
@@ -2,8 +2,40 @@ package com.example.lisa.datensammlerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class Datensammlung extends AppCompatActivity {
+
+
+    private void initGuiElements(){
+        Switch swchAc = findViewById(R.id.swchAc);
+        Switch swchGy = findViewById(R.id.swchGy);
+        Switch swchLo = findViewById(R.id.swchLo);
+        Switch swchLi = findViewById(R.id.swchLi);
+        Switch swchRe = findViewById(R.id.swchRe);
+
+        TextView acValX = findViewById(R.id.acValX);
+        TextView acValY = findViewById(R.id.acValX);
+        TextView acValZ = findViewById(R.id.acValX);
+
+        TextView gyValX = findViewById(R.id.gyValX);
+        TextView gyValY = findViewById(R.id.gyValX);
+        TextView gyValZ = findViewById(R.id.gyValX);
+
+        TextView loValLo = findViewById(R.id.loValLo);
+        TextView loValLa = findViewById(R.id.loValLa);
+
+        TextView liValCa = findViewById(R.id.liValCa);
+
+        EditText acHz = findViewById(R.id.acHz);
+        EditText gyHz = findViewById(R.id.gyHz);
+        EditText loHz = findViewById(R.id.loHz);
+        EditText liHz = findViewById(R.id.liHz);
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
